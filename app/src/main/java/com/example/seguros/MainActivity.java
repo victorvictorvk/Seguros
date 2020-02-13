0acb5b494c4f27d1e906577901954b1489069335
victorvictorvk <lavialactea89@hotmail.com>
2020-02-13 22:20:42 +0100

    Creación del color picker

--- app/src/main/java/com/example/seguros/MainActivity.java
+++ app/src/main/java/com/example/seguros/MainActivity.java
@@ -4,39 +4,56 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.PopupMenu;
 import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.content.ContextCompat;
 
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import yuku.ambilwarna.AmbilWarnaDialog;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText edUsuario;
     EditText edPass;
+    ConstraintLayout fondo;
+    int mDefaultColor;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         edUsuario = (EditText) findViewById(R.id.editTextUsuario);
         edPass = (EditText) findViewById(R.id.editTextPass);
+        fondo = (ConstraintLayout) findViewById(R.id.fondo);
+        mDefaultColor = ContextCompat.getColor(MainActivity.this, R.color.colorPrimary);
 
         establecerFondo();
 
     }
     public void establecerFondo(){
-        SharedPreferences prefs = getSharedPreferences("fondoAplicacion", Context.MODE_PRIVATE);
+        //Leemos el fichero para saber qué color es el que hay que coger.
+        SharedPreferences prefs = getSharedPreferences("fondoAplicaciones", Context.MODE_PRIVATE);
         ConstraintLayout fondo = (ConstraintLayout) findViewById(R.id.fondo);
 
-        String nombreFondo = prefs.getString("fondo", "rojo");
+        String nombreFondo = prefs.getString("fondo", "personalizado");
+        mDefaultColor = prefs.getInt("numeroColores", 0);
+
+
         if( nombreFondo.equals("azul"))
         {
             fondo.setBackgroundResource(R.mipmap.fondo_azul_3);
@@ -44,9 +61,11 @@ public class MainActivity extends AppCompatActivity {
         }else if( nombreFondo.equals("amarillo")) {
             fondo.setBackgroundResource(R.mipmap.fondo_amarillo);
 
-        }else {
+        }else if( nombreFondo.equals("rojo")){
             fondo.setBackgroundResource(R.mipmap.fondo_rojo);
-
+        }else if( nombreFondo.equals("personalizado"))
+        {
+            fondo.setBackgroundColor(mDefaultColor);
         }
     }
     public void menuPopup(View v)
@@ -59,7 +78,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public boolean onMenuItemClick(MenuItem item) {
 
-                ConstraintLayout fondo = (ConstraintLayout) findViewById(R.id.fondo);
+                SharedPreferences preferencias = getSharedPreferences("fondoAplicaciones", Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor = preferencias.edit();
                 String nombreFondo  = "azul";
                 switch (item.getItemId()){
                     case R.id.MnuOpc1:
@@ -69,29 +89,62 @@ public class MainActivity extends AppCompatActivity {
                     case R.id.SubMnuOpc1:
                         fondo.setBackgroundResource(R.mipmap.fondo_azul_3);
                         nombreFondo = "azul";
+                        editor.putString("fondo", nombreFondo);
                         break;
+
                     case R.id.SubMnuOpc2:
                         fondo.setBackgroundResource(R.mipmap.fondo_amarillo);
                         nombreFondo = "amarillo";
-
+                        editor.putString("fondo", nombreFondo);
                         break;
+
                     case R.id.SubMnuOpc3:
                         fondo.setBackgroundResource(R.mipmap.fondo_rojo);
                         nombreFondo = "rojo";
-
+                        editor.putString("fondo", nombreFondo);
                         break;
 
+                    case R.id.SubMnuOpc4:
+                        editor.putInt("numeroColores", openColorPicker());
+                        editor.putString("fondo", "personalizado");
+                        break;
                 }
-
-                SharedPreferences preferencias = getSharedPreferences("fondoAplicacion", Context.MODE_PRIVATE);
-                SharedPreferences.Editor editor = preferencias.edit();
-                editor.putString("fondo", nombreFondo);
                 editor.commit();
                 return true;
             }
         });
         menu.show();
 
+    }
+        public void mostar(View v)
+        {
+            SharedPreferences prefs = getSharedPreferences("fondoAplicaciones", Context.MODE_PRIVATE);
+            ConstraintLayout fondo = (ConstraintLayout) findViewById(R.id.fondo);
+
+            int colores = prefs.getInt("numeroColores", 0);
+            Button b = (Button) findViewById(R.id.button);
+            TextView tv = (TextView) findViewById(R.id.textView);
+            tv.setText("");
+            tv.setText(String.valueOf(colores));
+        }
+    public int openColorPicker() {
+
+        AmbilWarnaDialog colorPicker = new AmbilWarnaDialog(this, mDefaultColor, new AmbilWarnaDialog.OnAmbilWarnaListener() {
+            int mDefaultColor;
+
+            @Override
+            public void onCancel(AmbilWarnaDialog dialog) {
+
+            }
+
+            @Override
+            public void onOk(AmbilWarnaDialog dialog, int color) {
+                mDefaultColor = color;
+                fondo.setBackgroundColor(mDefaultColor);
+            }
+        });
+        colorPicker.show();
+        return mDefaultColor;
     }
 
     //Usaremos este método para guardar la información en el caso en que pase a segundo plano la App
